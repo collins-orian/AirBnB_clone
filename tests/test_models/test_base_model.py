a3a0fb7180fee1e8b0b76e2135abff6b2be2448c
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2023-02-14 00:35:37 +0100

    Update test_base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,40 @@
+from models.base_model import BaseModel
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    '''test BaseModel'''
+
+    def setUp(self):
+        self.base_modal = BaseModel()
+
+    def test_id(self):
+        self.assertIsInstance(self.base_modal.id, str)
+
+    def test_to_dict(self):
+        # Test that to_dict returns a dictionary
+        self.assertIsInstance(self.base_model.to_dict(), dict)
+
+        # Test that the dictionary has the correct keys and values
+        expected_dict = {
+            'id': self.base_model.id,
+            'created_at': self.base_model.created_at.isoformat(),
+            'updated_at': self.base_model.updated_at.isoformat(),
+            '__class__': 'BaseModel'
+        }
+        self.assertDictEqual(self.base_model.to_dict(), expected_dict)
+
+    def test_save(self):
+        # Test that calling save updates updated_at
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(old_updated_at, self.base_model.updated_at)
+
+    def test___str__(self):
+        expected_output = "[{}] ({}) {}".format(
+            self.base_modal.__class__.__name__, self.base_modal.id, self.base_modal.__dict__)
+        self.assertEqual(str(self.base_modal), expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
