7726dac162f881045fc01f45edf10031ccee907d
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2023-02-13 22:51:23 +0100

    updates to the project

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,22 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel with unique id and current datetime."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if args is not None and args > 0:
+            pass
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Print BaseModel instance information."""
