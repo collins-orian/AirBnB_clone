097125757248ea7f12895169b7f7e3004c5bfd02
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2023-02-13 17:04:24 +0100

    Create base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+'''Module for Base'''
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines common attributes and methods for other classes."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel with unique id and current datetime."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Print BaseModel instance information."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary of instance attributes."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
