20a8ab680668117a3fb909a532ff231af0f21512
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2023-04-08 13:16:14 +0100

    Create console.py

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains the entry point of the command interpreter.
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """This class defines the command interpreter."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, args):
+        """EOF command to exit the program."""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
